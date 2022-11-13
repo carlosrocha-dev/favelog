a539c4e453b67d715be46d60c1a53f8cb25c788a
robsoninoue <robson.inoue@gmail.com>
2022-11-13 20:13:49 -0300

    fixed prisma client

--- pages/api/clientPrisma.ts
+++ pages/api/clientPrisma.ts
@@ -8,12 +8,13 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+// if (process.env.NODE_ENV === 'production') {
+  // prisma = new PrismaClient()
+// } else {
+  // if (!global.prisma) {
+    // global.prisma = new PrismaClient()
+  // }
+  // prisma = global.prisma
+// }
+prisma = new PrismaClient
 export default prisma
