01ac163ad7cadac03a4e543f1e50f4b15b0e0598
Carlos Rocha <carlos.rocha01@gmail.com>
2022-11-14 11:03:23 -0300

    UPDATE

--- pages/cadastro.tsx
+++ pages/cadastro.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import CreateCourier from '../src/components/createCourier'
+import CreateRetail from '../src/components/createRetail'
 import styles from '../styles/Home.module.css'
 
 export default function Signin() {
