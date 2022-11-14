9b857d1682b81f67fb8d3f0b837f65662ce8060f
robsoninoue <robson.inoue@gmail.com>
2022-11-14 12:53:18 -0300

    restored db url

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,14 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { env } from 'process'
-
-// type Data = {
-//   name: string
-// }
 
 export default function test(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const test = env.DATABASE_URL
-    res.status(200).json(test)
+    res.status(200).json('its works')
 }
