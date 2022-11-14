f8b3b532c335f9e58d4ccbc5bbd35e826e8f1601
Carlos Rocha <carlos.rocha01@gmail.com>
2022-11-14 07:47:03 -0300

    feature: teste logo no index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Favelog</title>
+        <title>Favelog - conectando a comunidade</title>
         <meta name="description" content="Aplicativo para Integração entre os microempreendedores da favela e moto-taxis, para serviços de entrega e variados" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-         Favelog
+      <h1 className={styles.title}>
+          <Image
+             src="img/logo.png"
+             alt="favelog"
+             layout="responsive"
+          />
         </h1>
         <div className="backgorund"></div>
         <p className={styles.description}>
