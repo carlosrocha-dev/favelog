857084e1073767bd2c30fd147c6426323a7fa0a9
Carlos Rocha <carlos.rocha01@gmail.com>
2022-11-14 06:39:29 -0300

    feature: home test

--- pages/home.tsx
+++ pages/home.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home2() {
   return (
     <div className={styles.container}>
       <Head>
