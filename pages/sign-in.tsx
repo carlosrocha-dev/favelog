eaae2cfd7fb0f821ff5575156871779203ba9f97
Carlos Rocha <carlos.rocha01@gmail.com>
2022-11-14 06:48:10 -0300

    feature: updata componet image logo

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home2() {
+export default function Signin() {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,27 +12,15 @@ export default function Home2() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         Favelog
+          <Image
+             src="img/logo.png"
+             alt="favelog"
+          />
         </h1>
         <div className="backgorund"></div>
         <input required />
         <button>Cadastrar</button>
-        <img src="img/logo.png" alt="favelog" />
 
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
